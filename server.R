b2a55bfc8ce89fb8b2294941c202f6a8c27f356b
Nick Fanton <51746234+n-fanton@users.noreply.github.com>
2021-12-26 15:42:30 +0000

    add comments and clean up code

--- server.R
+++ server.R
@@ -1,21 +1,24 @@
 # App server side calculations ------------------------------------------------
 server <- function(input, output) {
-  ## Live departures table-----------------------------------------------------
-  ### Current time for refresh button -----------------------------------------
+  ## Current time for refresh button ------------------------------------------
   output$last_refreshed <- renderText({
     input$refresh
     paste0("Last refreshed: ", current_time())
   })
-  ### Download live departures data -------------------------------------------
+
+  ## Download live departures data for selected stops -------------------------
   live_departures_data <- reactive({
+    # Reload when refresh button is clicked
     input$refresh
 
+    # Find IDs for selected bus stops to read coming departures
     selected_stop_ids <- stop_lookups %>%
       filter(search_name %in% input$selected_stops) %>%
       select(stop_id) %>%
       pull()
 
-    #### Download data from API if services are selected ----------------------
+    # Download data from API if services are selected, otherwise return
+    # empty tibble
     if (length(selected_stop_ids) > 0) {
       live_departures(selected_stop_ids)
     } else {
@@ -23,60 +26,87 @@ server <- function(input, output) {
     }
   })
 
-  ### Data table to show live departures --------------------------------------
+  ## Data table to show live departures ---------------------------------------
   output$live_departures <- renderDataTable({
-    #### Find how much time it's been since midnight --------------------------
-    # This is used to calculate the number of minutes until arrival
+    # Find how much time it's been since midnight
+    # Minutes until arrival are calculated as the difference in minutes since
+    # midnight of the last refresh, and of the time the bus is due. This also
+    # allows for post-midnight services: if requested time is in the 'past',
+    # e.g. 01:30 when it's currently 11:20, it'll add 24h to the 01:30.
     minutes_since_midnight <- Sys.time() %>%
       str_sub(12,16) %>%
       to_minutes()
 
-    #### Output table ---------------------------------------------------------
+    ### Output table ----------------------------------------------------------
     live_departures_data() %>%
+      # Filter out terminating services unless selected otherwise in input
       {if (input$show_terminators) {
         .
       } else if ("is_terminating_here" %in% names(.)) {
+        # If there's no terminating services the variable doesn't appear in
+        # the API data, so only filtering if the column is present
         filter(., !is_terminating_here)
       }} %>%
+      # Add stop ID data for display
       left_join(stop_lookups, by = c("sms" = "stop_id")) %>%
       mutate(
+        # The tram route is listed as T50 (ambitious!), so here I'm changing
+        # it to just say 'Tram'
         route_name = case_when(route_name == "T50" ~ "Tram",
                                TRUE ~ route_name),
+        # Find minutes since midnight of departures
         time_after_midnight = to_minutes(display_time),
+        # Calculate departure time in minutes since current time
         departs_in = time_after_midnight - minutes_since_midnight,
+        # Sometimes the departure time shows as -1, so here it shows as 'DUE'
+        # if due in less than a minute
         due = case_when(departs_in < 1 ~ "DUE",
                         TRUE ~ as.character(departs_in)),
         destination = case_when(
+          # This typo has been annoying me for the three years I've lived
+          # in Edinburgh
           destination == "Royal Infirmry" ~ "Royal Infirmary",
-          TRUE ~ destination),
-        t_diversion = case_when(is_diverted ~ "Diverted. ",
-                                TRUE ~ ""),
-        t_term = case_when(is_terminating_here ~ "Terminates here. ",
-                           TRUE ~ ""),
-        note = paste0(t_term, t_diversion)) %>%
+          TRUE ~ destination)) %>%
+      # Sort order of display by due time, route, and destination
       arrange(departs_in, route_name, display_name) %>%
+      # Select only variables that make it to output
       select(due, route_name, destination, display_name) %>%
-      rename(`Route` = route_name,
+      # Rename into human-friendly variable names
+      # Importatnt to display stop because multiple stops can be selected,
+      # and display_name also includes identifiers where there's multiple
+      # stops grouped together (Princes Street, RIE, etc.)
+      rename(`Due` = due,
+             `Route` = route_name,
              `To` = destination,
-             `Due` = due,
              `Stop` = display_name)
   },
+  # Only show eight entries per page of output
   options = list(pageLength = 8))
 
-  ### Put output into a wrapper if there's no data to display -----------------
+  ## Put output into a wrapper if there's no data to display ------------------
   output$departures_ui <- renderUI({
+    # If there are no upcoming departures, just return the text
     if (nrow(live_departures_data()) == 0)
-      return("No data to show")
+      return("No departures")
 
+    # If there are some departures to display, show them!
     dataTableOutput("live_departures")
   })
 
   ## Create live map ----------------------------------------------------------
   output$services_map <- renderLeaflet({
 
+    # Refresh this when the refresh button is clicked
     input$refresh
 
+    # Create baseline map to which to add elements
+    map <- leaflet() %>%
+      addProviderTiles("Esri.WorldTopoMap")
+
+    ### Display bus routes ----------------------------------------------------
+    #### Select routes to display ---------------------------------------------
     if (input$show_all) {
+      # Display all routes if that's what's requested
       selected_services <- stop_lookups %>%
         filter(search_name %in% input$selected_stops) %>%
         select(services) %>%
@@ -84,35 +114,41 @@ server <- function(input, output) {
         unlist() %>%
         unique()
     } else if (nrow(live_departures_data()) > 0) {
+      # If not displaying all routes, display those on departures board,
+      # if there are any
       selected_services <- live_departures_data() %>%
         select(route_name) %>%
         pull() %>%
         unique()
     } else {
-      selected_services <- NA
+      # If not displaying all, and departures board is empty, return NULL
+      selected_services <- NULL
     }
 
-    map <- leaflet() %>%
-      addProviderTiles("Esri.WorldTopoMap")
-
-    if (suppressWarnings(!is.na(selected_services)) &
-        length(selected_services) > 0) {
+    #### Add services to map --------------------------------------------------
+    if (length(selected_services) > 0) {
+      # If there are services to display, add them to the base map
       map <- map %>%
         add_services_to_map(services = selected_services,
                             shp = shapefiles)
     } else {
+      # If there are no services to display, just add a note that says so
       map <- map %>%
         addAwesomeMarkers(
-          icon = icons,
+          icon = awesomeIcons(
+            icon = "bus",
+            iconColor = "red",
+            library = "fa",
+            markerColor = "white",
+            iconRotate = 180),
           lat = 55.951741,
           lng = -3.191745,
           label = "One day, there will be buses")
     }
 
-    if (input$show_stops &
-        suppressWarnings(!is.na(selected_services)) &
-        length(selected_services) > 0) {
-
+    #### Add bus stops to map (if requested) ----------------------------------
+    if (input$show_stops & length(selected_services) > 0) {
+      # Find all stops used by the selected routes
       stops_to_display <- stops_by_route %>%
         filter(services %in% selected_services) %>%
         select(stop_id) %>%
@@ -120,14 +156,17 @@ server <- function(input, output) {
         unlist() %>%
         unique()
 
+      # Get extra data for the selected stops: all routes that call there,
+      # all destinations, and where the stops actually are
+      # TODO: Show all stops in group rather than just the exact stops
       stop_map_data <- stops %>%
         filter(stop_id %in% stops_to_display) %>%
         mutate(stop_label = paste0(
           "<b>", display_name, "</b><br><b> Routes: </b>",
           display_services, "<br><b> To: </b>",
-          display_destinations
-        ))
+          display_destinations))
 
+      # Add the stop data to map, with custom markers
       map <- map %>%
         addAwesomeMarkers(
           icon = awesomeIcons(
@@ -138,26 +177,33 @@ server <- function(input, output) {
           data = stop_map_data,
           lat = ~latitude,
           lng = ~longitude,
+          # Without lapply, each stop will show data for all
+          # see https://stackoverflow.com/questions/43144596
           label = ~lapply(stop_label, htmltools::HTML),
           clusterOptions = markerClusterOptions(
             maxClusterRadius = 33))
-    }
+      }
 
-    if (input$show_live_buses) {
+    #### Display live bus locations on map (if requested)
+    if (input$show_live_buses & length(selected_services) > 0) {
+      # Load live locations from TfE and then filter for just the services
+      # we want displayed on the map
       temp_locations <- live_locations() %>%
         filter(service_name %in% selected_services) %>%
         left_join(stop_lookups, by = c("next_stop_id" = "stop_id")) %>%
         left_join(route_colours, by = c("service_name" = "name")) %>%
         mutate(
+          # Want to avoid displaying NAmph or 'Moving at 0mph'
           display_speed = case_when(
             is.na(speed) ~ "Stopped",
             speed == 0 ~ "Stopped",
             TRUE ~ paste0("Moving at ", speed, "mph")),
           bus_label = paste0(
             "Service <b>", service_name, "</b> to <b>", destination, "</b><br>",
-            "Next stop: ", display_name, "<br>", display_speed))
+            "Next stop: ", display_name, "<br>", display_speed, "<br>",
+            last_gps_fix_secs, " seconds ago"))
 
-
+      # Add the bus locations to the base map
       map <- map %>%
         addAwesomeMarkers(
           data = temp_locations,
@@ -169,29 +215,44 @@ server <- function(input, output) {
             iconColor = colour,
             library = "fa",
             markerColor = "white",
+            # Rotate the arrow marker for the direction the bus is heading
             iconRotate = heading),
           clusterOptions = markerClusterOptions(
             maxClusterRadius = 33))
+      }
 
-    }
-
+    ### Return the map --------------------------------------------------------
+    # This really feels like to small a command after all the buildup
     map
+    })
 
-  })
-
+  ## Travel alerts ------------------------------------------------------------
   output$alerts <- renderUI({
-    httr::GET(url = "https://lothianupdates.com/api/public/getServiceUpdates?key=8094E98541294E7AC25491127FAC7A72") %>%
+    # Travel alerts link - I got this directly from the Lothian website
+    alerts_url <- paste0("https://lothianupdates.com/api/public/",
+                         "getServiceUpdates")
+
+
+    # The alerts are returned as JSON, but all I really need is one single
+    # bit of HTML-styled text to display
+    # First read the JSON data and clean it up a bit
+    httr::GET(url = alerts_url) %>%
       httr::content(as = "text", encoding = "UTF-8") %>%
       jsonlite::fromJSON(flatten = TRUE) %>%
       magrittr::extract2(1) %>%
       tibble::as_tibble() %>%
       janitor::clean_names() %>%
-      mutate(label = paste0("<b>", title_en, "</b><br>", description_en, "<br>")) %>%
+      # New variable to display alerts styled in HTML, bold title, normal
+      # text, a new row for each title and bit of text
+      mutate(label = paste0("<b>", title_en, "</b><br>",
+                            description_en, "<br>")) %>%
+      # Now select just this column and extract it into one long string
       select(label) %>%
       pull() %>%
       paste(collapse = "") %>%
+      # Finally, apply HTML style to what is now just text with weird
+      # <b> and <br> tags in it
       htmltools::HTML()
-
-  })
-}
+    })
+  }
 
--- ui.R
+++ ui.R
@@ -5,81 +5,127 @@ source("app.R")
 header <- dashboardHeader(
   title = "Edinburgh bus times",
   titleWidth = "25%"
+  # 25% corresponds to the 3/12 width of the settings column in app
 )
 
 body <- dashboardBody(
   fluidRow(
-    column(#style = "position: fixed; overflow: visible;",
-           width = 3,
-           h2("Settings"),
-           box(
-             width = NULL,
-             status = "info",
-             selectizeInput(
-               inputId = "selected_stops",
-               label = "Select bus stops",
-               choices = search_names,
-               multiple = TRUE,
-               selected = "Royal Infirmary"),
-             checkboxInput(
-               inputId = "show_terminators",
-               label = "Show services that terminate here",
-               value = FALSE
-               )),
-           box(
-             width = NULL,
-             status = "info",
-             p(strong("Map settings")),
-             checkboxInput(
-               inputId = "show_all",
-               label = "Show all services that use this stop",
-               value = FALSE
-             ),
-             helpText(
-               "If unselected, only services displayed in the live",
-               "departures table will be shown"),
-             checkboxInput(
-               inputId = "show_stops",
-               label = "Show bus stops on map",
-               value = FALSE
-             ),
-             checkboxInput(
-               inputId = "show_live_buses",
-               label = "Show live bus locations",
-               value = TRUE
-             )),
-           box(
-             width = NULL,
-             status = "warning",
-             helpText(
-               "Table will refresh when new stops are selected, or when the",
-               "refresh button is clicked."),
-             textOutput("last_refreshed"),
-             actionButton(
-               inputId = "refresh",
-               label = "Refresh")),
-           h2("Travel alerts"),
-           box(
-             width = NULL,
-             status = "danger",
-             htmlOutput("alerts")
-           )),
-    column(width = 9,
-           h2("Live departures"),
-           box(
-             status = "success",
-             width = NULL,
-             uiOutput("departures_ui")
-           ),
-           h2("Available services"),
-           p("Tram services are currently not displayed on the map."),
-           box(
-             status = "success",
-             width = NULL,
-             leafletOutput("services_map",
-                           height = "800px")
-           )))
-)
+    column(
+      ## Sidebar column with settings and options -----------------------------
+      width = 3,
+      h2("Settings"),
+      box(
+        ### Box with settings for departures display --------------------------
+        width = NULL,
+        status = "info",
+        selectizeInput(
+          # Allows for multiple stops to be selected; defaults to RIE
+          inputId = "selected_stops",
+          label = "Select bus stops",
+          choices = search_names,
+          multiple = TRUE,
+          selected = "Royal Infirmary"
+          ),
+        checkboxInput(
+          # TfE API returns services that terminate, which may not be useful
+          inputId = "show_terminators",
+          label = "Show services that terminate here",
+          value = FALSE
+          )
+        ),
+      box(
+        ### Box with settings for map display - doesn't change table ----------
+        width = NULL,
+        status = "info",
+        p(strong("Map settings")),
+        helpText(
+          "If unselected, only services displayed in the live",
+          "departures table will be shown"
+          ),
+        checkboxInput(
+          # If selected, services that use this stop but aren't currently
+          # on the departure board will also be shown (e.g. nightbus
+          # services during the day).
+          inputId = "show_all",
+          label = "Show all services that use this stop",
+          value = FALSE
+          ),
+        checkboxInput(
+          # Display bus stops on map? Will display all stops used by the
+          # displayed services, and give info on routes and destinations
+          # available at the stop.
+          inputId = "show_stops",
+          label = "Show bus stops",
+          value = FALSE
+          ),
+        checkboxInput(
+          # Read realtime location data from TfE and display bus locations,
+          # route, destination, speed, direction, and next stop
+          inputId = "show_live_buses",
+          label = "Show live bus locations",
+          value = TRUE
+          )
+        ),
+      box(
+        ### Box for refreshing departures data and map ------------------------
+        # Shiny doesn't continuously read new data, so after a while it needs
+        # and update.
+        # TODO: automatically refresh after 60 seconds
+        width = NULL,
+        status = "warning",
+        p(strong("Refresh data")),
+        helpText(
+          "Table will refresh when new stops are selected, or when the",
+          "refresh button is clicked."),
+        actionButton(
+          inputId = "refresh",
+          label = "Refresh"
+          ),
+        textOutput("last_refreshed")
+        ),
+      ## Part 2 of the sidebar - travel alerts --------------------------------
+      # Potentially move below map instead, this takes a lot of room
+      # on mobile displays
+      h2("Travel alerts"),
+      box(
+        ### Box displaying travel alerts read form Lothian Buses API ----------
+        width = NULL,
+        status = "danger",
+        htmlOutput("alerts")
+      )),
+    column(
+      ## Main part of the panel, displaying output ----------------------------
+      width = 9,
+      h2("Live departures"),
+      box(
+        ### Table displaying next departures from selected stop(s) ------------
+        status = "success",
+        width = NULL,
+        uiOutput("departures_ui")
+      ),
+      h2("Services at selected stop"),
+      p("Some services may not currently display on the map."),
+      box(
+        ### Map output displaying services from the selected stop -------------
+        status = "success",
+        width = NULL,
+        leafletOutput("services_map",
+                      height = "800px")
+        ),
+      h2("Notes"),
+      box(
+        ### Add various notes etc. to the app
+        status = "info",
+        width = NULL,
+        p("When buses are stopped, their direction arrows will point up",
+          "as the direction cannot be determined."),
+        p("Timetable, location, stops, and routes data from Transport for",
+          "Edinburgh. Travel alerts and route colours from Lothian Buses."),
+        p("App author: Nick Fanton.")
+        )
+      )
+    )
+  )
 
 
 dashboardPage(
