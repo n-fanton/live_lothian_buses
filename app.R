d419657684b9e1678a81676cc14b7b82bada6e62
Nick Fanton <51746234+n-fanton@users.noreply.github.com>
2022-01-16 18:54:08 +0000

    use geojson and sf package in app

--- app.R
+++ app.R
@@ -6,6 +6,7 @@ library("janitor")
 library("shinythemes")
 library("shinyWidgets")
 library("leaflet")
+library("sf")
 
 # Intro: data -----------------------------------------------------------------
 
@@ -16,6 +17,7 @@ stops        <- read_rds(here::here("data", "stops.rds"))
 stops_by_route <- read_rds(here::here("data", "stops_by_route.rds"))
 route_colours <- read_rds(here::here("data", "route_colours.rds"))
 tram_shapefile <- read_rds(here::here("data", "tram_shapefile.rds"))
+routes_shapefile <- st_read(here::here("data", "tfe_shapefile.geojson"))
 
 
 # Remove Shandwick Place from display for trams
